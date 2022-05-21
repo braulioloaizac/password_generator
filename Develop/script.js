62042863b912c73db1858e769909a4b4e4036386
braulioloaizac <bloaizac@uqvirtual.edu.co>
2022-05-21 17:48:23 -0400

    first commit

---
+++ Develop/script.js
@@ -0,0 +1,19 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  // add your logic here
+  return;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
