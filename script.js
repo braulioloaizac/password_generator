ead04f0661fda435fcdc25cdbb9c35920cf95826
braulioloaizac <bloaizac@uqvirtual.edu.co>
2022-05-26 16:27:58 -0400

    changed files from directory

--- script.js
+++ script.js
@@ -18,13 +18,13 @@ function generatePassword() {
   
 
   // Asks about the password length
-  var pwdLenghtstr = window.prompt("Enter the password lenght (number between 8 and 128)")
+  var pwdLengthstr = window.prompt("Enter the password lenght (number between 8 and 128)")
   
   // String to integer conversion
-  var pwdLenght = parseInt(pwdLenghtstr);
+  var pwdLength = parseInt(pwdLengthstr);
 
   // Compares the entereed number
-  if (pwdLenght >= 8 && pwdLenght <= 128){
+  if (pwdLength >= 8 && pwdLength <= 128){
     
     // Asks about the password propieties
     var upper = window.confirm("Do you want uppercase letters on your password?");
@@ -36,116 +36,114 @@ function generatePassword() {
       if ((upper===false)&&(lower===false)&&(numeric===false)&&(spechar===true)){
         var availableChars = specialChars;
         var avClenght = specialCharsLenght;
-        password = passwordGenAuto(pwdLenght, availableChars, avClenght);
-
+        password = passwordGenAuto(pwdLength, availableChars, avClenght);
       }
       //0010
       else if ((upper===false)&&(lower===false)&&(numeric===true)&&(spechar===false)){
         var availableChars = numbersChars;
         var avClenght = numbersLenght;
-        password = passwordGenAuto(pwdLenght, availableChars, avClenght);
+        password = passwordGenAuto(pwdLength, availableChars, avClenght);
       }
       //0011
       else if ((upper===false)&&(lower===false)&&(numeric===true)&&(spechar===true)){
         var availableChars = numbersChars.concat(specialChars);
         avClenght = numbersLenght + specialCharsLenght;
-        password = passwordGenAuto(pwdLenght, availableChars, avClenght);
+        password = passwordGenAuto(pwdLength, availableChars, avClenght);
       }
 
       //0100
       else if ((upper===false)&&(lower===true)&&(numeric===false)&&(spechar===false)){
         var availableChars = lowerChars;
         avClenght = charsLenght;
-        password = passwordGenAuto(pwdLenght, availableChars, avClenght);
+        password = passwordGenAuto(pwdLength, availableChars, avClenght);
       }
 
       //0101
       else if ((upper===false)&&(lower===true)&&(numeric===false)&&(spechar===true)){
         var availableChars = lowerChars.concat(specialChars);
         avClenght = charsLenght + specialCharsLenght;
-        password = passwordGenAuto(pwdLenght, availableChars, avClenght);
+        password = passwordGenAuto(pwdLength, availableChars, avClenght);
       }
 
      //0110
      else if ((upper===false)&&(lower===true)&&(numeric===true)&&(spechar===false)){
       var availableChars = lowerChars.concat(numbersChars);
       avClenght = charsLenght + numbersLenght;
-      password = passwordGenAuto(pwdLenght, availableChars, avClenght);
+      password = passwordGenAuto(pwdLength, availableChars, avClenght);
     } 
 
     //0111
     else if ((upper===false)&&(lower===true)&&(numeric===true)&&(spechar===true)){
       var availableChars = lowerChars.concat(numbersChars,specialChars);
       avClenght = charsLenght + numbersLenght + specialCharsLenght;
-      password = passwordGenAuto(pwdLenght, availableChars, avClenght);
+      password = passwordGenAuto(pwdLength, availableChars, avClenght);
     } 
 
     //1000
     else if ((upper===true)&&(lower===false)&&(numeric===false)&&(spechar===false)){
       var availableChars = upperChars;
       avClenght = charsLenght;
-      password = passwordGenAuto(pwdLenght, availableChars, avClenght);
+      password = passwordGenAuto(pwdLength, availableChars, avClenght);
     }
 
     //1001
     else if ((upper===true)&&(lower===false)&&(numeric===false)&&(spechar===true)){
       var availableChars = upperChars.concat(specialChars);
       avClenght = charsLenght + specialCharsLenght;
-      password = passwordGenAuto(pwdLenght, availableChars, avClenght);
+      password = passwordGenAuto(pwdLength, availableChars, avClenght);
     }
 
     //1010
     else if ((upper===true)&&(lower===false)&&(numeric===true)&&(spechar===false)){
       var availableChars = upperChars.concat(numbersChars);
       avClenght = charsLenght + numbersLenght;
-      password = passwordGenAuto(pwdLenght, availableChars, avClenght);
+      password = passwordGenAuto(pwdLength, availableChars, avClenght);
     }
 
     //1011
     else if ((upper===true)&&(lower===false)&&(numeric===true)&&(spechar===true)){
       var availableChars = upperChars.concat(numbersChars,specialChars);
       avClenght = charsLenght + numbersLenght + specialCharsLenght;
-      password = passwordGenAuto(pwdLenght, availableChars, avClenght);
+      password = passwordGenAuto(pwdLength, availableChars, avClenght);
     }
 
     //1100
     else if ((upper===true)&&(lower===true)&&(numeric===false)&&(spechar===false)){
       var availableChars = upperChars.concat(lowerChars);
       avClenght = charsLenght * 2;
-      password = passwordGenAuto(pwdLenght, availableChars, avClenght);
+      password = passwordGenAuto(pwdLength, availableChars, avClenght);
     }
 
     //1101
     else if ((upper===true)&&(lower===true)&&(numeric===false)&&(spechar===true)){
       var availableChars = upperChars.concat(lowerChars,specialChars);
       avClenght = (charsLenght * 2) + specialCharsLenght;
-      password = passwordGenAuto(pwdLenght, availableChars, avClenght);
+      password = passwordGenAuto(pwdLength, availableChars, avClenght);
     }
 
     //1110
     else if ((upper===true)&&(lower===true)&&(numeric===true)&&(spechar===false)){
       var availableChars = upperChars.concat(lowerChars,numbersChars);
       avClenght = (charsLenght * 2) + numbersLenght;
-      password = passwordGenAuto(pwdLenght, availableChars, avClenght);
+      password = passwordGenAuto(pwdLength, availableChars, avClenght);
     }
 
     //1111
     else if ((upper===true)&&(lower===true)&&(numeric===true)&&(spechar===true)){
       var availableChars = upperChars.concat(lowerChars,numbersChars, specialChars);
       avClenght = (charsLenght * 2) + numbersLenght + specialCharsLenght;
-      password = passwordGenAuto(pwdLenght, availableChars, avClenght);
+      password = passwordGenAuto(pwdLength, availableChars, avClenght);
     }
 
     else{
-      window.alert("You didn't select a valid option");
+      window.alert("You didn't select any option");
     }
+  }
 
-    }
+  else{
+    window.alert("You didn't select a valid option");
+  }
 
-    else{
-      window.alert("You didn't select a valid option");
-    }
-    console.log(password);
   return password;
 }
 
@@ -164,9 +162,9 @@ function getRndInteger(min, max) {
 }
 
 
-function passwordGenAuto(pwdLenght, availableChars, avClenght){
+function passwordGenAuto(pwdLength, availableChars, avClenght){
 
-  for (var i = 0; i<pwdLenght; i++){
+  for (var i = 0; i<pwdLength; i++){
           
     var numgen = getRndInteger(0,(avClenght-1));
     password = prevpass.concat(availableChars.charAt(numgen));
